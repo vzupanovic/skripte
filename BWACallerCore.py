9664f74521eab9b153b271c41ae33ccbb189ada6
vzupanovic <vanessa.zupanovic@fer.hr>
2014-01-24 00:01:58 +0100

    prepravljam

--- BWACallerCore.py
+++ BWACallerCore.py
@@ -11,14 +11,24 @@ from pylab import *
 from BWACaller import *
 
 class CallerCore:
-	def __init__(self, readType, readFile_1, readFile_2, contigFile, plotPath, showPlot):
+	def __init__(self, sysArgs):
+		if os.path.isfile(sysArgs[1])!=True:
+			print "[AN:] File "+str(sysArgs[1])+" doesn't exist. Exiting..."
+			exit(-1)
+		if os.path.isfile(sysArgs[2])!=True:
+			print "[AN:] File "+str(sysArgs[2])+" doesn't exist. Exiting..."
+			exit(-1)
+		if os.path.isfile(sysArgs[3])!=True:
+			print "[AN:] File "+str(sysArgs[1])+" doesn't exist. Exiting..."
+			exit(-1)
 		self.maxCovReg = [-1,-1,-1,-1]
-		self.readType = readType
-		self.readFile_1 = readFile_1
-		self.readFile_2 = readFile_2
-		self.contigFile = contigFile
-		self.plotPath = plotPath
-		self.showPlot = showPlot
+		self.readType = int(sysArgs[5])
+		self.readFile_1 = sysArgs[1]
+		self.readFile_2 = sysArgs[2]
+		self.contigFile = sysArgs[3]
+		self.plotPath = sysArgs[4]
+		self.showPlot = int(sysArgs[6])
+		
 		
 	def doBWA(self, regularPath):
 		self.bwa = BWACaller(self.readType, self.contigFile, self.readFile_1, self.readFile_2, regularPath)
@@ -64,26 +74,10 @@ if __name__ == "__main__":
 		print "[AN:] Usage:\n\tpython BWACallerCore.py read1.fastq read2.fastq[null]\n\tcontig_file.fasta plot_folder read_type[0/1] show_plot[0/1]"
 		print "\tpath_to_data_folder[~/example/] path_to_BEDTOOLS[~/bedtools/bin/bedtools]" 
 	else:
-		if os.path.isfile(sys.argv[1])!=True:
-			print "[AN:] File "+str(sys.argv[1])+" doesn't exist. Exiting..."
-			exit(-1)
-		if os.path.isfile(sys.argv[2])!=True:
-			print "[AN:] File "+str(sys.argv[2])+" doesn't exist. Exiting..."
-			exit(-1)
-		if os.path.isfile(sys.argv[3])!=True:
-			print "[AN:] File "+str(sys.argv[1])+" doesn't exist. Exiting..."
-			exit(-1)
-
-		read1 = sys.argv[1]
-		read2 = sys.argv[2]
-		contigFile = sys.argv[3]
-		plotFolder = sys.argv[4]
-		readType = int(sys.argv[5])	
-		showPlot = int(sys.argv[6])
 		regularPath = sys.argv[7]
 		pathToBedTools = sys.argv[8]
 		#regularPath = "/home/lexy/BWA_test/"
-		core = CallerCore(readType, read1, read2, contigFile, plotFolder, showPlot)
+		core = CallerCore(sys.argv)
 		core.doBWA(regularPath)
 		core.doSAM(regularPath)
 		core.doBAM(regularPath, pathToBedTools)
